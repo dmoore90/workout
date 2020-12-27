c5842a537f10199100cd45d7cdff00fd180384c1
r34p3r <danieljmoore123@gmail.com>
2020-12-26 18:14:21 -0800

    completed test.py for read write alteration of workoutlist and data.txt

--- test.py
+++ test.py
@@ -1,30 +1,23 @@
 #! /usr/bin/env python3
 
-# def wList(x, w):
-# 	f = open("data.txt", "a")
-# 	# exercise = input("enter ex: ")
-# 	f.write("{}".format(x))
-# 	# weight = input("enter weight: ")
-# 	f.write("{}\n".format(w))
-# 	f.close()
+def wList(x, w):
+	f = open("data.txt", "a")
+	# exercise = input("enter ex: ")
+	f.write("{}: ".format(x))
+	# weight = input("enter weight: ")
+	f.write("{}\n".format(w))
+	f.close()
 
-# exercises = {
-# 	"situps: ": "bodyweight", "chinups: ": "bodyweight", 
-# 	"overhead press: ": "bodyweight", "squats: ":"bodyweight", 
-# 	"bench press: ": "bodyweight", "deadlift: ": "bodyweight",
-# 	"---":"----"
-# 	}
+workoutdict = {}
 
-
-
-def readValues():
+def readVal():
 	ex = ""
 	w = ""
 	lines = []
 	lines1 = []
 	exercises = []
 	weights = []
-	workoutdict = {}
+	wd = {}
 	newLine = ""
 	f = open("data.txt", "r")
 	for i in f.readlines():
@@ -48,55 +41,62 @@ def readValues():
 				w = ""
 		weights.append(w)
 		w + ""
-				
-	print(weights)
-	print("---")
-	print(exercises)
+	i = 0
+	while (i < len(exercises)):
+		workoutdict[exercises[i]] = weights[i]
+		i += 1
 
-readValues()
 
-# def changeVal():
-# 	a = ""
-# 	while (a != "*"):
-# 		print("Press 1 for situps weight: ")
-# 		print("Press 2 for chinups weight: ")
-# 		print("Press 3 for overheadpress weight: ")
-# 		print("press 4 for squats: ")
-# 		print("Press 5 for bench press weight: ")
-# 		print("Press 6 for deadlift: ")
-# 		print("Press * to quit:")
-# 		a = input("Enter option number: ")
-# 		if (a == "1"):
-# 			print("true")
-# 			for k,v in exercises.items():
-# 				if (k == "situps: "):
-# 					exercises["situps"] = input("enter new weight: ")
-# 		elif (a == "2"):
-# 			for k,v in exercises.items():
-# 				if (k == "chinups: "):
-# 					v = input("enter new weight: ")
-# 		elif (a == "3"):
-# 			for k,v in exercises.items():
-# 				if (k == "overhead press: "):
-# 					v = input("enter new weight: ")
-# 		elif (a == "4"):
-# 			for k,v in exercises.items():
-# 				if (k == "squats: "):
-# 					v = input("enter new weight: ")
-# 		elif (a == "5"):
-# 			for k,v in exercises.items():
-# 				if(k == "bench press: "):
-# 					v = input("enter new weight: ")
-# 		elif (a == "6"):
-# 			for k,v in exercises.items():
-# 				if (k == "deadlift: "):
-# 					v = input("enter new weight: ")
-# 		elif (a == "*"):
-# 			a = "*"
 
-# def printList():
-# 	for k,v in exercises.items():
-# 		wList(k,v)
+def changeVal():
+	a = ""
+	while (a != "*"):
+		for k,v in workoutdict.items():
+			print(k, v)
+		print("Press 1 for situps weight: ")
+		print("Press 2 for chinups weight: ")
+		print("Press 3 for overheadpress weight: ")
+		print("press 4 for squats: ")
+		print("Press 5 for bench press weight: ")
+		print("Press 6 for deadlift: ")
+		print("Press * to quit:")
+		a = input("Enter option number: ")
+		if (a == "1"):
+			print("true")
+			for k,v in workoutdict.items():
+				if (k == "situps"):
+					workoutdict[k] = input("enter new weight: ")
+		elif (a == "2"):
+			for k,v in workoutdict.items():
+				if (k == "chinups"):
+					workoutdict[k] = input("enter new weight: ")
+		elif (a == "3"):
+			for k,v in workoutdict.items():
+				if (k == "overhead press"):
+					workoutdict[k] = input("enter new weight: ")
+		elif (a == "4"):
+			for k,v in workoutdict.items():
+				if (k == "squats"):
+					workoutdict[k] = input("enter new weight: ")
+		elif (a == "5"):
+			for k,v in workoutdict.items():
+				if(k == "bench press"):
+					workoutdict[k] = input("enter new weight: ")
+		elif (a == "6"):
+			for k,v in workoutdict.items():
+				if (k == "deadlift"):
+					workoutdict[k] = input("enter new weight: ")
+		elif (a == "*"):
+			a = "*"
 
-# printList()
-# changeVal()
+def printList():
+	for k,v in workoutdict.items():
+		wList(k,v)
+	f = open("data.txt", "a")
+	f.write("\n")
+	f.closed
+
+readVal()
+changeVal()
+printList()
+
